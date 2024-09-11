74f1f213f3a98f59781c0718dbf20f4ed150ef61
omad_d <odilshodvekov13@gmail.com>
2024-09-12 00:58:20 +0500

    Offline mode and PWA support for Android/IOS is added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "favicon.ico", "apple-touch-icon.png", "android-chrome-512x512.png", "android-chrome-192x192.png"],
+      manifest: {
+        name: "Map Application",
+        short_name: "Map",
+        description:
+          "A map application with markers on it containing status and comments",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
 })
